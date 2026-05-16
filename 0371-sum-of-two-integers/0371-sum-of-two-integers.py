a3d881349a3e28c4a83539c66a78904d378eb50d
Sajjad_AIDATA <145702159+Sajjad371@users.noreply.github.com>
2026-05-16 21:32:34 +0500

    Time: 0 ms (100%), Space: 12.5 MB (0.51%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getSum(self, a, b):
+        return sum([a,b])
+        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna

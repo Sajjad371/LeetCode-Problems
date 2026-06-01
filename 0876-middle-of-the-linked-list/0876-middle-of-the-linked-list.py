e81d6ba34cb22fff3e8934842dc505e20545f52f
Sajjad_AIDATA <145702159+Sajjad371@users.noreply.github.com>
2026-06-01 21:04:11 +0500

    Time: 0 ms (100%), Space: 12.5 MB (24.17%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        next = head
+        next1 = head
+        while next1 and next1.next:
+            next = next.next       
+            next1 = next1.next.next
+        return next
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
